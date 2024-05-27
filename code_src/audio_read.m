1a56e888743f9662c2963a2fc7bb164c0d952b9d
Immortal-Fates <1877114891@qq.com>
2024-05-27 23:09:49 +0800

    feat: achieve resample audio signal

--- code_src/audio_read.m
+++ code_src/audio_read.m
@@ -1,3 +1,6 @@
+close all;
+clf; clear; clc; %清空工作区
+
 load ./mat_src/filter_design.mat
 %TODO: 读取音频文件
 % file = '..\audio_src\1-9男无(1).wav'; %读取文件
@@ -61,5 +64,92 @@ f_base = (maxIndex - 1) * FS / length(s1_filter);
 disp(['The base frequency is: ', num2str(f_base), ' Hz']);
 
 %%信号重构
-% 新基频
+% TODO:设置新基频
 f_new_base = 220; %220Hz
+
+% 采用重采样方法
+% 计算新的采样率
+FS_new = round(FS * (f_new_base / f_base));
+
+% 计算采样率的比例
+[p, q] = rat(FS / FS_new);
+
+% 使用新的采样率重采样
+s1_resampled = resample(s1_filter, p, q);
+
+% 播放重采样后的音频
+sound(s1_resampled, FS_new);
+% 将重采样后的音频保存到文件
+audiowrite('..\audio_output\啦啦啦男resampled_audio.wav', s1_resampled, FS_new);
+
+% %% 采用音高移位方法
+% % 创建音高移位器
+% shifter = audiopluginexample.PitchShifter;
+
+% % 计算音高移位的比例
+% shiftRatio = f_new_base / f_base;
+
+% % 设置音高移位的比例
+% shifter.PitchShift = shiftRatio;
+
+% % 对音频进行音高移位
+% s1_resampled = shifter(s1_filter);
+
+% % 播放音高移位后的音频
+% sound(s1_resampled, FS);
+
+% % 将音高移位后的音频保存到文件
+% audiowrite('..\audio_output\啦啦啦男shifted_audio.wav', s1_resampled, FS);
+
+
+% 重构音频的频域波形
+%%绘制信号频率信息
+s1_L = length(s1_resampled);
+% %原始信号频率信息
+% p = fft(s1); %计算傅里叶变换
+%滤波后信号频率信息
+p=fft(s1_resampled);
+
+nUniquePts = ceil((s1_L + 1) / 2);
+p1 = p(1:nUniquePts); %选择前半部，因为后半部是前半部的一个镜像
+p2 = abs(p1); %取绝对值，即幅度
+p3 = p2 / s1_L; %使用点数按比例缩放，这样幅度和信号长度或者它自身的频率无关
+p4 = p3 .^ 2; %平方得到功率
+
+if rem(s1_L, 2) %奇数，nfft需要排除奈奎斯特点
+    p4(2:end) = p4(2:end) * 2;
+else
+    p4(2:end - 1) = p4(2:end - 1) * 2;
+end
+
+sampFreq = FS;
+freqArray = (0:nUniquePts - 1) * (sampFreq / n); %创建频率数组
+figure; plot(freqArray / 1000000, 10 * log10(p4), 'k');
+title('Power-Frequency Curve');
+xlabel('Frequency(kHz)');
+ylabel('Power(dB)');
+% 计算频谱
+p = abs(fft(s1_resampled));
+
+% 找到最大幅度的频率
+[~, maxIndex] = max(p);
+
+% 计算基频
+f_base = (maxIndex - 1) * FS / length(s1_resampled);
+
+% 显示基频
+disp(['The base frequency is: ', num2str(f_base), ' Hz']);
+
+
+% 重构音频的时域波形
+file_refactor='..\audio_output\啦啦啦男shifted_audio.wav';
+%音频播放
+[Y, FS] = audioread(file_refactor);
+[m, n] = size(Y);
+s1 = Y(:, 1);
+s1_filter=filter(b,1,s1); %滤波
+
+%%音频绘制
+timeArray = (0:m - 1) / FS;
+timeArray1 = timeArray * 1000; %放大到毫秒级
+figure; plot(timeArray1, s1, 'k'); title('Amplitude Curve'); xlabel('Time(ms)'); ylabel('Amplitude');
--- code_src/filter_design.m
+++ code_src/filter_design.m
@@ -1,32 +1,5 @@
 close all;
 clf; clear; clc; %清空工作区
-%设计滤波器
-% % 设计参数
-% Fs = 1000;  % 采样频率
-% Fpass = 200;  % 通过频率
-% Fstop = 300;  % 停止频率
-% Apass = 1;  % 通过带最大损失（dB）
-% Astop = 60;  % 停止带最小衰减（dB）
-
-% % 设计滤波器
-% d = designfilt('lowpassfir', ...
-%   'PassbandFrequency', Fpass, 'StopbandFrequency', Fstop, ...
-%   'PassbandRipple', Apass, 'StopbandAttenuation', Astop, ...
-%   'DesignMethod', 'kaiserwin', 'SampleRate', Fs);
-
-% % 显示滤波器信息
-% fvtool(d);
-
-% %%汉明窗
-% N = 64; % 窗口长度
-% w = hamming(N); % 设计汉明窗
-
-% % 绘制汉明窗的形状
-% figure;
-% plot(w);
-% title('Hamming Window');
-% xlabel('Sample');
-% ylabel('Amplitude');
 
 %%采用汉明窗设计低通FIR滤波器
 file='..\audio_src\啦啦啦男无(1).wav';
@@ -34,7 +7,7 @@ file='..\audio_src\啦啦啦男无(1).wav';
 [Y, FS] = audioread(file);
 [m, n] = size(Y);
 % 设计参数
-Fpass = 500;  % 通过频率
+Fpass = 1000;  % 通过频率
 N = m; %采样点数
 
 % 计算归一化截止频率
